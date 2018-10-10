65b561fddaee99f7ecb13f235f4e743e44d97869
A.D.Faris <adfaris@gmail.com>
2018-10-10 16:33:26 -0400

    into_react

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Card.css';
+import React from 'react'
+import './Card.css'
+
+const CardBanner = () => {
+  return (
+    <a className="class-banner" href="https://reactjs.org">
+      <img
+        alt="logo"
+        className="banner-image"
+        src="https://ibin.co/3wnC6SgIOJud.png"
+      />
+    </a>
+  )
+}
